056626e0fc6687a66143965f927880d074994336
hernani Aleman Ferraz <afhernani@hotmail.com>
2020-02-29 02:24:09 +0000

    init status proyect kvthumb

---
+++ README.md
@@ -0,0 +1,21 @@
+# kvthumb
+## making of archive gif.
+## Autor: Hernani Aleman Ferraz
+## email: afhernani@gmail.com / hotmail.com
+
+# kvthumb
+## rite: python kvthumb.py
+
+# threds
+## it's content of thred elements.
+
+# LoadDialog.
+## select directory that content the subdirectory of pictures or directory of movies.
+
+# kvthumbApp.
+## Name of application in execution.
+
+# APP used or required
+## ffplayer
+### util to extract pictures elements of video.
+
---
+++ thumb.py
@@ -0,0 +1,110 @@
+# ! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os, sys
+import threading
+import time
+import kivy
+kivy.require('1.10.0')
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.factory import Factory
+from kivy.animation import Animation
+from kivy.clock import Clock, mainthread
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.metrics import sp
+
+
+class Thumb(BoxLayout):
+    def __init__(self, source, **kwargs):
+        super().__init__(**kwargs)
+        # si existe el fichero lo adicionamos, si no, 
+        # no lo adicionamos.
+        if os.path.exists(source):
+            self.ids.imgview.source=source
+            self.ids.label.text = source
+
+    def addate_image(self, th):
+        print('action_image:', th.encode('utf-8'))
+
+
+class ThumbView(BoxLayout):
+    stop = threading.Event()
+    files=[]
+    def __init__(self, files=[], **kwargs):
+        super().__init__(**kwargs)
+        self.files=files
+        for file in self.files:
+            self.ids.box.add_widget(Thumb(source=file))
+
+    def aplicar(self, *args):
+        print('aplicar:', args)
+
+class Mensaje(Label):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.size_hint = (1, None)
+        self.font_size = sp(20)
+
+    def on_size(self, *args):
+        self.text_size = (self.width-sp(10), None)
+
+    def on_texture_size(self, *args):
+        self.size = self.texture_size
+        self.height += sp(20)
+
+
+class ThumbApp(App):
+    def on_stop(self):
+        # The Kivy event loop is about to stop, set a stop signal;
+        # otherwise the app window will close, but the Python process will
+        # keep running until all secondary threads exit.
+        self.root.stop.set()
+
+    def build(self):
+        self.files=[]
+        # files = ['bbt.gif', 'huge.gif', 'kingy-anal.gif', 'mellons.gif', 'mother.gif']
+        directorio = 'F:\\tmp\\VSDG_E'
+        # self.thumbview = ThumbView(files=files)
+        self.thumbview = ThumbView()
+        self.load_thread(directorio)
+        return self.thumbview
+    
+    def load_thread(self, dirpath='.'):
+        from functools import partial
+        self.dirpathmovies = dirpath
+        self.title='Splitfloat :: ' + self.dirpathmovies
+        self.thumbview.ids.idpath.text = self.dirpathmovies
+        # print('dirpathmovies:', self.dirpathmovies)
+        exten = ('.gif', '.GIF')
+        dirthumbs = os.path.join(self.dirpathmovies, 'Thumbails')
+        # print('dirthumbs:', dirthumbs)
+        self.thumbview.ids.box.clear_widgets()
+        if os.path.exists(dirthumbs):
+            for fe in os.listdir(dirthumbs):
+                if fe.endswith(exten):
+                    fex = os.path.abspath(os.path.join(dirthumbs, fe))
+                    # print(fex.encode('utf-8'))
+                    self.files.append(fex)
+                    # Clock.schedule_once(partial(self.update_box_imagen, str(fex)))
+                    # self.box.ids.box.add_widget(Splitfloat(source=fex, anim_delay= 1))
+        threading.Thread(target=self.start_load_thread, args=(self.files,), daemon=True).start()
+        #print('files:', self.files,', len:', len(self.files))
+
+    def start_load_thread(self, files=[]):
+        try:
+            for file in files:
+                self.update_box_imagen(file)
+        except:
+            print('excepcion en start_load_thread')
+    
+    @mainthread
+    def update_box_imagen(self, file ):
+        self.thumbview.ids.box.add_widget(Thumb(source=file))
+        self.thumbview.ids.imgview.source = file
+
+
+if __name__ == '__main__':
+    ThumbApp().run()
