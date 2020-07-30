16e5f29b58d6709128f86fd9dcb152aa76b0e966
afhernani <afhernani@gmail.com>
2020-07-30 22:32:24 +0100

    add init archive clas Box

--- hpopup/__init__.py
+++ hpopup/__init__.py
@@ -19,7 +19,7 @@ try:
     from .notification import Notification, Message, Error, Confirmation
     from .progress import Progress, Loading
     from .form import HSlider, HTextInput, Notes, Authorization
-    from .archive import Copy, Move, Remove, Rename
+    from .archive import Copy, Move, Remove, Rename, Box
 except:
     from hpopup import HPopup
     from hbase import HBase
@@ -29,7 +29,7 @@ except:
     from notification import Notification, Message, Error, Confirmation
     from progress import Progress, Loading
     from form import HSlider, HTextInput, Notes, Authorization
-    from archive import Copy, Move, Remove, Rename
+    from archive import Copy, Move, Remove, Rename, Box
 
 __author__ = 'hernani'
 __version__ = '0.0.1'
